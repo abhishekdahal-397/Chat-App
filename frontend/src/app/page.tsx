e07709b1bc75d081d1cebe8bcd77654c496b7205
Abhishek Dahal <abhishekdahal397@gmail.com>
2024-10-06 14:00:54 +0545

    added the authentication full of errors

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,22 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
 
-export default function Home() {
+export default function Component() {
+	const { data: session } = useSession();
+	console.log(session);
+	if (session) {
+		return (
+			<>
+				Signed in as {session.user.email} <br />
+				<button onClick={() => signOut()}>Sign out</button>
+			</>
+		);
+	}
 	return (
-		<div>
-			<Button variant={"outline"} asChild>
-				<Link href="/register">register</Link>
-			</Button>
-		</div>
+		<>
+			Not signed in <br />
+			<button onClick={() => signIn("github")}>Sign in using Github</button>
+			<button onClick={() => signIn("google")}>Sign in using Google</button>
+		</>
 	);
 }
