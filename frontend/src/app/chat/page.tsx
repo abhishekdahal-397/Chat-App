b199e4bbdf4e0d1a5198720b2e1ab6437121bbb3
Abhishek Dahal <abhishekdahal397@gmail.com>
2024-10-19 14:10:38 +0545

    added post route and maintained ui

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,6 +1,12 @@
 "use client";
+import ChatBox from "@/ui/chatSection";
 import React from "react";
 
-export const page = () => {
-	return <div>page</div>;
-};
+export default function ChatPage() {
+	return (
+		<div className="bg-blue-800">
+			Authenticated user
+			<ChatBox />
+		</div>
+	);
+}
