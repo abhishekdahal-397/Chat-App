83ce9f6c7557cfef53b233802889b548ce521188
Abhishek Dahal <abhishekdahal397@gmail.com>
2024-10-21 21:56:45 +0545

    frontend clerk complete

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+	const user = await currentUser();
+	const username = user?.username;
+	console.log("username is ", username);
+
 	return (
 		<div>
+			<p>{username}</p>
 			<Button variant={"outline"} asChild>
 				<Link href="/register">register</Link>
 			</Button>
--- index.js
+++
@@ -1,6 +0,0 @@
-  authenticate[m
-* [32mfeature/room[m
-  master[m
-  passport-auth[m
-  register[m
-  ui/dashboard[m
