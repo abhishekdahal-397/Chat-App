4839ac1ff72a53aada0264d80d938049f8b74df5
Abhishek Dahal <abhishekdahal397@gmail.com>
2024-11-29 17:39:11 +0545

    some minor changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,8 @@
 const mongoose = require("mongoose");
-
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(
-			process.env.MONGO_URI || "mongodb://localhost:27017/chatapp",
-			{
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-			}
-		);
-		console.log(`MongoDB Connected: ${conn.connection.host}`);
+		const conn = await mongoose.connect(process.env.MONGODB_URI);
+		console.log(`MongoDB Connected: ${process.env.MONGODB_URI}`);
 	} catch (error) {
 		console.error(`Error: ${error.message}`);
 		process.exit(1); // Exit with failure
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const connectDB = require("./config/db");
+require("dotenv").config();
 
 const app = express();
 const router = express.Router();
@@ -18,6 +19,7 @@ app.use("/api/users", userRoutes);
 router.get("/", () => {
 	res.send("hello");
 });
+console.log("port is ", process.env.PORT);
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
